5a5c337664b6818001fc2f52319055fb1c9a536a
xiaoq87722-art <xiaoq87722@gmail.com>
2026-05-13 08:06:39 +0800

    Update test_kcp.py

--- examples/client/test_kcp.py
+++ examples/client/test_kcp.py
@@ -20,10 +20,10 @@ import time
 from ctypes import c_int, c_uint, c_long, c_void_p, CFUNCTYPE
 
 
-SERVER_HOST = '127.0.0.1'
+SERVER_HOST = '44.197.226.175'
 SERVER_PORT = 5555
-NUM_CLIENTS = 200
-DATA_SIZE   = 512
+NUM_CLIENTS = 100
+DATA_SIZE   = 4096
 TIMEOUT_SEC = 10.0
 
 
