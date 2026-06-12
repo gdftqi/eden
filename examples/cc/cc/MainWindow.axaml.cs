5194110a174d5d4a6c5a2869f08da12aff973415
xiaoq87722-art <xiaoq87722@gmail.com>
2026-06-12 17:53:03 +0800

    update

--- examples/cc/cc/MainWindow.axaml.cs
+++ examples/cc/cc/MainWindow.axaml.cs
@@ -1,4 +1,6 @@
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
 
 namespace cc
 {
@@ -7,6 +9,22 @@ namespace cc
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            if (Application.Current?.ApplicationLifetime is IControlledApplicationLifetime lifetime)
+            {
+                lifetime.Shutdown();
+            }
+        }
+
+        private void TopBar_PointerPressed(object? sender, Avalonia.Input.PointerPressedEventArgs e)
+        {
+            if (e.GetCurrentPoint(this).Properties.IsLeftButtonPressed)
+            {
+                this.BeginMoveDrag(e);
+            }
         }
     }
 }
