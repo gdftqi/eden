e56d7c072f7bb1eedc3b953af49276572c403456
xiaoq87722-art <xiaoq87722@gmail.com>
2026-06-26 11:06:05 +0800

    Update user_login.go

--- ra/handlers/user_login.go
+++ ra/handlers/user_login.go
@@ -161,7 +161,7 @@ func UserLogin(c *gin.Context) {
 	rsp := userLoginRsp{
 		Conv:   conv,
 		UserID: userID,
-		Host:   "172.26.29.158:5555",
+		Host:   "13.214.204.197:5555",
 		HostID: 1000,
 		MacKey: conf.Instance.SipHashKey,
 		Token:  base64.StdEncoding.EncodeToString(sealed),
