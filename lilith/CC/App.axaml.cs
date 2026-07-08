edd64e5a1277ee0d7b08591f357a3a8a1509df33
xiaoq87722-art <xiaoq87722@gmail.com>
2026-07-08 13:31:01 +0800

    update

--- lilith/CC/App.axaml.cs
+++ lilith/CC/App.axaml.cs
@@ -13,17 +13,32 @@ namespace CC
             AvaloniaXamlLoader.Load(this);
         }
 
+
+
         public override void OnFrameworkInitializationCompleted()
         {
+            /// HTTP 服务的 X25519 公钥
+            const string HTTP_X25519_PK = "LZBT82+6Hdzz/pqnOyk3tRh1460vhxVJ1NcvLT3kn0M=";
+
+            /// HTTP 服务地址
+            //public const string HTTP_HOST = "http://172.26.29.158:8080";
+            const string HTTP_HOST = "http://13.212.159.179:8080";
+
+            /// KCP 连接超时
+            const uint KCP_TIMEOUT = 30000;
+
+            /// 重连的最大时间
+            const uint RECONNECT_MAX_TIME = 300000;
+
             if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
             {
                 // 配置 Hydra(RA 地址/公钥、超时、重连预算由宿主注入)+ 接上 pump:
                 // KCP IO 线程唤醒 → 这里 marshal 到 UI 线程后调 Hydra.Update, 让所有回调落在 UI 线程。
                 Hydra.Instance
-                    .SetHttpBaseUrl(Config.HTTP_HOST)
-                    .SetHttpX25519PK(Config.HTTP_X25519_PK)
-                    .SetKcpTimeoutMs(Config.KCP_TIMEOUT)
-                    .SetReconnectMaxMs(Config.RECONNECT_MAX_TIME)
+                    .SetHttpBaseUrl(HTTP_HOST)
+                    .SetHttpX25519PK(HTTP_X25519_PK)
+                    .SetKcpTimeoutMs(KCP_TIMEOUT)
+                    .SetReconnectMaxMs(RECONNECT_MAX_TIME)
                     .SetonWakeup(() => Dispatcher.UIThread.Post(Hydra.Instance.Update));
 
                 desktop.MainWindow = new LoginWindow();
--- lilith/CC/Config.cs
+++ lilith/CC/Config.cs
@@ -9,22 +9,6 @@ namespace CC
 {
     internal class Config
     {
-        /// <summary>
-        /// HTTP 服务的 X25519 公钥
-        /// </summary>
-        public const string HTTP_X25519_PK = "LZBT82+6Hdzz/pqnOyk3tRh1460vhxVJ1NcvLT3kn0M=";
 
-        /// <summary>
-        /// HTTP 服务地址
-        /// </summary>
-        //public const string HTTP_HOST = "http://172.26.29.158:8080";
-        public const string HTTP_HOST = "http://13.212.159.179:8080";
-
-        /// <summary>
-        /// KCP 连接超时
-        /// </summary>
-        public const uint KCP_TIMEOUT = 30000;
-
-        public const uint RECONNECT_MAX_TIME = 300000;
     }
 }
