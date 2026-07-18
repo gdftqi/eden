4971a94a46dcde4225544c8fa5cf1a49b01519ef
xiaoq87722-art <xiaoq87722@gmail.com>
2026-07-19 04:08:38 +0800

    Update adam.h

--- Adam/include/adam.h
+++ Adam/include/adam.h
@@ -18,21 +18,14 @@ using TcpConf = adam::tcp::Conf;
 #define KCP_SERVER_MAIN(name, config, ...) \
     static adam::kcp::Server::Ptr server; static void on_signal(int) { if (server) server->stop(); } \
     int main(int /* argc */, char** /* argv */) { \
-        if (!adam::utils::lock_pid(#name ".pid")) { \
-            xERROR("程序已启动"); \
-            return EXIT_FAILURE; \
-        } \
+        if (!adam::utils::lock_pid(#name ".pid")) { xERROR("程序已启动"); return EXIT_FAILURE; } \
         KcpConf::instance()->load_from_file(config); \
         adam::utils::init_log(KcpConf::instance()->log_path()); \
         adam::utils::Prof prof(KcpConf::instance()->prof_path()); \
-        name se; \
-        server = std::make_unique<adam::kcp::Server>(&se); \
+        name se; server = std::make_unique<adam::kcp::Server>(&se); \
         __VA_ARGS__ \
-        ::signal(SIGINT, on_signal); \
-        ::signal(SIGTERM, on_signal); \
-        server->run(); \
-        xINFO("服务关闭"); \
-        return EXIT_SUCCESS; \
+        ::signal(SIGINT, on_signal); ::signal(SIGTERM, on_signal); \
+        server->run(); xINFO("服务关闭"); return EXIT_SUCCESS; \
     }
 
 
