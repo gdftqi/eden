617f6cdb5175cbb321f09e8bde9e5cc6d9b003fb
xiaoq87722-art <xiaoq87722@gmail.com>
2026-07-29 08:01:58 +0800

    Update Sys.cs

--- Lilith/CC/Utils/Sys.cs
+++ Lilith/CC/Utils/Sys.cs
@@ -1,6 +1,4 @@
-using Avalonia;
-using Avalonia.Media.Imaging;
-using Avalonia.Platform;
+using SkiaSharp;
 using System;
 using System.IO;
 using System.Runtime.InteropServices;
@@ -11,35 +9,35 @@ namespace CC.Utils
     public class Sys
     {
 #if WINDOWS
-        [DllImport("user32.dll")]
+        [DllImport("user32.dll", ExactSpelling = true)]
         private static extern IntPtr GetDesktopWindow();
 
-        [DllImport("user32.dll")]
+        [DllImport("user32.dll", SetLastError = true, ExactSpelling = true)]
         private static extern IntPtr GetWindowDC(IntPtr hWnd);
 
-        [DllImport("user32.dll")]
+        [DllImport("user32.dll", ExactSpelling = true)]
         private static extern int ReleaseDC(IntPtr hWnd, IntPtr hDC);
 
-        [DllImport("gdi32.dll")]
+        [DllImport("gdi32.dll", SetLastError = true, ExactSpelling = true)]
         private static extern bool BitBlt(IntPtr hdcDest, int nXDest, int nYDest, int nWidth, int nHeight, IntPtr hdcSrc, int nXSrc, int nYSrc, int dwRop);
 
-        [DllImport("gdi32.dll")]
+        [DllImport("gdi32.dll", SetLastError = true, ExactSpelling = true)]
         private static extern IntPtr CreateCompatibleDC(IntPtr hDC);
 
-        [DllImport("gdi32.dll")]
+        [DllImport("gdi32.dll", SetLastError = true, ExactSpelling = true)]
         private static extern IntPtr CreateCompatibleBitmap(IntPtr hDC, int nWidth, int nHeight);
 
-        [DllImport("gdi32.dll")]
+        [DllImport("gdi32.dll", ExactSpelling = true)]
         private static extern IntPtr SelectObject(IntPtr hDC, IntPtr hObject);
 
-        [DllImport("gdi32.dll")]
+        [DllImport("gdi32.dll", ExactSpelling = true)]
         private static extern bool DeleteDC(IntPtr hdc);
 
-        [DllImport("gdi32.dll")]
+        [DllImport("gdi32.dll", ExactSpelling = true)]
         private static extern bool DeleteObject(IntPtr hObject);
 
-        [DllImport("gdi32.dll")]
-        private static extern bool GetDIBits(IntPtr hdc, IntPtr hbmp, uint uStartScan, uint cScanLines, [Out] byte[] lpvBits, ref BITMAPINFO lpbmi, uint uUsage);
+        [DllImport("gdi32.dll", SetLastError = true, ExactSpelling = true)]
+        private static extern int GetDIBits(IntPtr hdc, IntPtr hbmp, uint uStartScan, uint cScanLines, [Out] byte[] lpvBits, ref BITMAPINFO lpbmi, uint uUsage);
 
         [StructLayout(LayoutKind.Sequential)]
         private struct BITMAPINFOHEADER
@@ -61,51 +59,106 @@ namespace CC.Utils
         private struct BITMAPINFO
         {
             public BITMAPINFOHEADER bmiHeader;
-            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
-            public byte[] bmiColors;
+            public uint mask0;
+            public uint mask1;
+            public uint mask2;
         }
 
-        static public void CaptureDesktop(int width, int height)
+        public static void CaptureDesktop(int width, int height)
         {
-            IntPtr hSrcDC = GetWindowDC(GetDesktopWindow());
-            IntPtr hDestDC = CreateCompatibleDC(hSrcDC);
-            IntPtr hBitmap = CreateCompatibleBitmap(hSrcDC, width, height);
-            IntPtr hOldBmp = SelectObject(hDestDC, hBitmap);
+            if (!OperatingSystem.IsWindows())
+                throw new PlatformNotSupportedException("桌面截图目前仅支持 Windows");
 
-            // 屏幕拷贝 (SRCCOPY)
-            BitBlt(hDestDC, 0, 0, width, height, hSrcDC, 0, 0, 0x00CC0020);
+            const int SRCCOPY = 0x00CC0020;
+            const int CAPTUREBLT = 0x40000000;   // 带上分层窗口
 
-            BITMAPINFO bmi = new BITMAPINFO();
-            bmi.bmiHeader.biSize = (uint)Marshal.SizeOf(typeof(BITMAPINFOHEADER));
-            bmi.bmiHeader.biWidth = width;
-            bmi.bmiHeader.biHeight = -height;
-            bmi.bmiHeader.biPlanes = 1;
-            bmi.bmiHeader.biBitCount = 32;
+            IntPtr hDesktop = GetDesktopWindow();
+            IntPtr hSrcDC = IntPtr.Zero, hDestDC = IntPtr.Zero;
+            IntPtr hBitmap = IntPtr.Zero, hOldBmp = IntPtr.Zero;
 
-            byte[] rawPixels = new byte[width * height * 4];
-            GetDIBits(hDestDC, hBitmap, 0, (uint)height, rawPixels, ref bmi, 0);
-
-            SelectObject(hDestDC, hOldBmp);
-            DeleteObject(hBitmap);
-            DeleteDC(hDestDC);
-            ReleaseDC(GetDesktopWindow(), hSrcDC);
-
-            var pixelSize = new PixelSize(width, height);
-            var dpi = new Vector(96, 96);
-            var avaloniaBitmap = new WriteableBitmap(pixelSize, dpi, PixelFormat.Bgra8888, AlphaFormat.Premul);
-
-            using (var lockedBuffer = avaloniaBitmap.Lock())
+            try
             {
-                Marshal.Copy(rawPixels, 0, lockedBuffer.Address, rawPixels.Length);
-            }
+                hSrcDC = GetWindowDC(hDesktop);
+                if (hSrcDC == IntPtr.Zero)
+                {
+                    return;
+                }
 
-            string savePath = Path.Combine(AppContext.BaseDirectory, "desktop_native.png");
-            avaloniaBitmap.Save(savePath);
+                hDestDC = CreateCompatibleDC(hSrcDC);
+                hBitmap = CreateCompatibleBitmap(hSrcDC, width, height);
+                if (hDestDC == IntPtr.Zero || hBitmap == IntPtr.Zero)
+                {
+                    return;
+                }
+
+                hOldBmp = SelectObject(hDestDC, hBitmap);
+
+                if (!BitBlt(hDestDC, 0, 0, width, height, hSrcDC, 0, 0, SRCCOPY | CAPTUREBLT))
+                {
+                    return;
+                }
+
+                // 关键:取位之前先把 bitmap 从 DC 摘出来
+                SelectObject(hDestDC, hOldBmp);
+                hOldBmp = IntPtr.Zero;
+
+                var bmi = new BITMAPINFO();
+                bmi.bmiHeader.biSize = (uint)Marshal.SizeOf<BITMAPINFOHEADER>();
+                bmi.bmiHeader.biWidth = width;
+                bmi.bmiHeader.biHeight = -height;   // 负值 = 自上而下,和 Skia 一致
+                bmi.bmiHeader.biPlanes = 1;
+                bmi.bmiHeader.biBitCount = 32;
+                bmi.bmiHeader.biCompression = 0;         // BI_RGB
+
+                var rawPixels = new byte[width * height * 4];
+                int scanned = GetDIBits(hDestDC, hBitmap, 0, (uint)height, rawPixels, ref bmi, 0);
+                if (scanned != height)
+                {
+                    return;
+                }
+
+                var buf = EncodeBgraToPng(rawPixels, width, height);
+                Save(buf, "1.png");
+            }
+            finally
+            {
+                if (hOldBmp != IntPtr.Zero)
+                {
+                    SelectObject(hDestDC, hOldBmp);
+                }
+
+                if (hBitmap != IntPtr.Zero)
+                {
+                    DeleteObject(hBitmap);
+                }
+
+                if (hDestDC != IntPtr.Zero)
+                {
+                    DeleteDC(hDestDC);
+                }
+
+                if (hSrcDC != IntPtr.Zero)
+                {
+                    ReleaseDC(hDesktop, hSrcDC);
+                }
+            }
         }
 #elif MACOS
 // TODO MacOS
 #else
 // TODO Linux
 #endif
+        public static byte[] EncodeBgraToPng(byte[] bgra, int width, int height, int quality = 80)
+        {
+            var info = new SKImageInfo(width, height, SKColorType.Bgra8888, SKAlphaType.Opaque);
+            using var image = SKImage.FromPixelCopy(info, bgra);
+            using var data = image.Encode(SKEncodedImageFormat.Png, quality);
+            return data.ToArray();
+        }
+
+        public static void Save(byte[] buf, string path)
+        {
+            File.WriteAllBytes(path, buf);
+        }
     }
 }
